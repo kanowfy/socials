ce6da3ecab952dcf1e83503cd90c96fd455d7c1e
kanowfy <nguyendung2002hl@gmail.com>
2023-08-22 14:04:38 +0700

    first commit

---
+++ src/main/java/com/vc/socials/SocialsApplication.java
@@ -0,0 +1,13 @@
+package com.vc.socials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vc/socials/SocialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vc.socials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
